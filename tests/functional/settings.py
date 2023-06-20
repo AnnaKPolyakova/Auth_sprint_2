91ba272a71671db7f56ae76f7562c2d727a8b2ee
Анна Полякова <Sova040888@yandex.ru>
2023-06-20 14:37:29 +0300

    fix actions

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -3,7 +3,7 @@ from logging import config as logging_config
 from pathlib import Path
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 from tests.functional.logger import LOGGING
 
@@ -14,17 +14,21 @@ load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env_test"), override=True)
 
 
 class TestSettings(BaseSettings):
-    REDIS_HOST: str
-    REDIS_PORT: str
-    REDIS_PROTOCOL: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
-    POSTGRES_DB: str
-    POSTGRES_DB_TEST: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    JWT_SECRET_KEY: str = "test"
-    DEFAULT_LIMITS: int
+    REDIS_HOST: str = Field(env="REDIS_HOST", default='redis')
+    REDIS_PORT: str = Field(env="REDIS_HOST", default='6379')
+    REDIS_PROTOCOL: str = Field(env="REDIS_HOST", default='redis')
+
+    POSTGRES_HOST: str = Field(env="REDIS_HOST", default='db')
+    POSTGRES_PORT: int = Field(env="REDIS_HOST", default='5432')
+    POSTGRES_DB: str = Field(env="REDIS_HOST", default='auth')
+    POSTGRES_DB_TEST: str = Field(env="REDIS_HOST", default='auth_test')
+    POSTGRES_USER: str = Field(env="REDIS_HOST", default='app')
+    POSTGRES_PASSWORD: str = Field(env="REDIS_HOST", default='123qwe')
+    JWT_SECRET_KEY: str = Field(
+        env="REDIS_HOST",
+        default='Y0QMIGwksa5OhtOBF9BczuAJ0hYMUv7esEBgMMdAuJ4V7stwxT9e'
+    )
+    DEFAULT_LIMITS: int = Field(env="REDIS_HOST", default=100)
 
     class Config:
         env_file = os.path.join(BASE_DIR, ".env", ".env_test")
