a1b7f6b12f2f7d200597b0545807b89100c4e356
Анна Полякова <Sova040888@yandex.ru>
2023-06-20 15:02:56 +0300

    fix actions

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -15,20 +15,19 @@ load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env_test"), override=True)
 
 class TestSettings(BaseSettings):
     REDIS_HOST: str = Field(env="REDIS_HOST", default='redis')
-    REDIS_PORT: str = Field(env="REDIS_HOST", default='6379')
-    REDIS_PROTOCOL: str = Field(env="REDIS_HOST", default='redis')
-
+    REDIS_PORT: str = Field(env="REDIS_PORT", default='6379')
+    REDIS_PROTOCOL: str = Field(env="REDIS_PROTOCOL", default='redis')
     POSTGRES_HOST: str = Field(env="REDIS_HOST", default='db')
-    POSTGRES_PORT: int = Field(env="REDIS_HOST", default=5432)
-    POSTGRES_DB: str = Field(env="REDIS_HOST", default='auth')
-    POSTGRES_DB_TEST: str = Field(env="REDIS_HOST", default='auth_test')
-    POSTGRES_USER: str = Field(env="REDIS_HOST", default='app')
-    POSTGRES_PASSWORD: str = Field(env="REDIS_HOST", default='123qwe')
+    POSTGRES_PORT: int = Field(env="POSTGRES_PORT", default=5432)
+    POSTGRES_DB: str = Field(env="POSTGRES_DB", default='auth')
+    POSTGRES_DB_TEST: str = Field(env="POSTGRES_DB_TEST", default='auth_test')
+    POSTGRES_USER: str = Field(env="POSTGRES_USER", default='app')
+    POSTGRES_PASSWORD: str = Field(env="POSTGRES_PASSWORD", default='123qwe')
     JWT_SECRET_KEY: str = Field(
-        env="REDIS_HOST",
+        env="JWT_SECRET_KEY",
         default='Y0QMIGwksa5OhtOBF9BczuAJ0hYMUv7esEBgMMdAuJ4V7stwxT9e'
     )
-    DEFAULT_LIMITS: int = Field(env="REDIS_HOST", default=100)
+    DEFAULT_LIMITS: int = Field(env="DEFAULT_LIMITS", default=100)
 
     class Config:
         env_file = os.path.join(BASE_DIR, ".env", ".env_test")
